e4dc46c310fb3db8a688607654475385a70d2bbb
davihr1 <davihenriquefavaro@gmail.com>
2022-10-24 23:09:22 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.tsx",
+        "./index.html"
+    ],
+    theme: {
+        fontFamily: {
+          'sans': ['Rubik', 'sans-serif'],
+        },
+        backgroundImage: {
+          galaxy: "url(./background-galaxy.png)",
+        }
+      },
+    plugins: [],
+  }
+  
